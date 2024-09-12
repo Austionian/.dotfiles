4ed309936f09f71e06a3e268b4c20b6e451f1170
Austin Rooks <Austionian@users.noreply.github.com>
2024-09-11 22:39:57 -0500

    add just file syntax highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -608,7 +608,8 @@ require("lazy").setup({
 		end
 	},
 	-- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	{'vmchale/just-vim'},
 })
 
 --[[
